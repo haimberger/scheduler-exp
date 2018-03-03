01a30366e2f4ba1a1242fb765192d5797b343571
Terese Haimberger <haimberger@fitanalytics.com>
2018-03-03 20:47:45 +0100

    use react-router

--- web/src/scheduler/Scheduler.js
+++ web/src/scheduler/Scheduler.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './Scheduler.css';
 
-class Scheduler extends Component {
-  render() {
-    return (
-      <div className="Scheduler">
-        <div className="Scheduler-tasks"></div>
-        <a href="#pending">Show pending tasks...</a>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="Home">
+    <div className="Home-tasks"></div>
+    <Link to="/pending">Show pending tasks...</Link>
+  </div>
+);
+
+const Pending = () => (
+  <div className="Pending">
+    <div className="Pending-tasks"></div>
+    <Link to="/">Back</Link>
+  </div>
+);
+
+const Scheduler = () => (
+  <Router>
+    <div className="Scheduler">
+      <Route exact path="/" component={Home}/>
+      <Route path="/pending" component={Pending}/>
+    </div>
+  </Router>
+);
 
 export default Scheduler;
